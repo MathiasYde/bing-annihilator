ebc89836cf128e5bc62ac0b7f94fbe53d7e8348e
MathiasYde <35201354+MathiasYde@users.noreply.github.com>
2020-07-26 22:01:24 +0200

    Update main.js
    Fixed a bug where it would redirect any new tabs to Google.com

--- bing-annihilator/main.js
+++ bing-annihilator/main.js
@@ -1,13 +1,14 @@
 
 
 chrome.tabs.onCreated.addListener((tab) => {
-  newUrl = new URL(tab.pendingUrl);
+  try {
+    newUrl = new URL(tab.pendingUrl);
 
-  //Check if the newly created tab directs to bing and has a search parameter
-  if (newUrl.hostname !== "www.bing.com" && newUrl.search === "") {
-    return null;
+    //Check if the newly created tab directs to bing and has a search parameter
+    if (newUrl.hostname === "www.bing.com" && newUrl.search !== "") {
+      //Change domain to google.com with the search parameter
+      chrome.tabs.update(tab.id, {url: `https://www.google.com/search${new URL(tab.pendingUrl).search}`});
+    }
   }
-
-  //Change domain to google.com with the search parameter
-  chrome.tabs.update(tab.id, {url: `https://www.google.com/search${new URL(tab.pendingUrl).search}`});
+  catch (error) {}
 });
